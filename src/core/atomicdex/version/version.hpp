d8da92192fe7e81243c6b6e8ff6951c20fdd5ef0
Alexander Khudyakov <89903767+Miner113@users.noreply.github.com>
2022-11-24 09:20:23 +0700

    0.5.71

--- src/core/atomicdex/version/version.hpp
+++ src/core/atomicdex/version/version.hpp
@@ -21,24 +21,24 @@ namespace atomic_dex
     constexpr const char*
     get_version()
     {
-        return "0.5.7-beta";
+        return "0.5.71-beta";
     }
 
     constexpr int
     get_num_version() noexcept
     {
-        return 57;
+        return 571;
     }
 
     constexpr const char*
     get_raw_version()
     {
-        return "0.5.7";
+        return "0.5.71";
     }
 
     constexpr const char*
     get_precedent_raw_version()
     {
-        return "0.5.6.1";
+        return "0.5.61.1";
     }
 } // namespace atomic_dex
